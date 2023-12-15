c41fd351789cf394cc490e585c3622f97eeb0822
Sora <tsoora@proton.me>
2023-12-16 02:07:47 +0800

    Using GoFiber as BackEnd

--- artefak.go
+++ artefak.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/theartefak/inertia-fiber"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/theartefak/inertia-fiber"
+)
 
 func main() {
-    println(inertia.Inertia())
+	app := fiber.New()
+
+	app.Use(cors.New())
+	app.Use(csrf.New())
+	app.Use(helmet.New())
+	app.Use(limiter.New())
+	app.Use(logger.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString(inertia.Inertia())
+	})
+
+    app.Listen("127.0.1.1:8000")
 }
