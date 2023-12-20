a202c35383a99d644f491678db5085ac9a3c481b
Sora <tsoora@proton.me>
2023-12-20 14:01:38 +0800

    Move to Bootstrap

--- artefak.go
+++ artefak.go
@@ -2,40 +2,17 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/gofiber/fiber/v2/middleware/csrf"
-	"github.com/gofiber/fiber/v2/middleware/favicon"
-	"github.com/gofiber/fiber/v2/middleware/helmet"
-	"github.com/gofiber/fiber/v2/middleware/limiter"
-	"github.com/gofiber/fiber/v2/middleware/logger"
-	"github.com/theartefak/inertia-fiber"
+	"github.com/theartefak/artefak/bootstrap"
 )
 
 func main() {
-	engine := inertia.New()
+	app := bootstrap.Run()
 
-	artefak := fiber.New(fiber.Config{
-		Views: engine,
-	})
-
-	artefak.Use(favicon.New(favicon.Config{
-		File: "./public/favicon.ico",
-	    URL: "/favicon.ico",
-	}))
-
-	artefak.Use(cors.New())
-	artefak.Use(csrf.New())
-	artefak.Use(helmet.New())
-	artefak.Use(limiter.New())
-	artefak.Use(logger.New())
-	artefak.Use(engine.Middleware())
-	artefak.Static("/assets", "public/build/assets")
-
-	artefak.Get("/", func(c *fiber.Ctx) error {
+	app.Get("/", func(c *fiber.Ctx) error {
 		return c.Render("Index", fiber.Map{
 			"greeting": "Hello World",
 		})
 	}).Name("Welcome")
 
-    artefak.Listen("127.0.1.1:8000")
+    app.Listen("127.0.1.1:8000")
 }
---
+++ bootstrap/runner.go
@@ -0,0 +1,34 @@
+package bootstrap
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/csrf"
+	"github.com/gofiber/fiber/v2/middleware/favicon"
+	"github.com/gofiber/fiber/v2/middleware/helmet"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/theartefak/inertia-fiber"
+)
+
+func Run() *fiber.App {
+	engine := inertia.New()
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Use(favicon.New(favicon.Config{
+		File: "./public/favicon.ico",
+	}))
+
+	app.Use(cors.New())
+	app.Use(csrf.New())
+	app.Use(engine.Middleware())
+	app.Use(helmet.New())
+	app.Use(limiter.New())
+	app.Use(logger.New())
+	app.Static("/assets", "public/build/assets")
+
+	return app
+}
