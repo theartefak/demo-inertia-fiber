a4c592c61dbb6f3ff2b521d71c5add656e75eed5
Sora <tsoora@proton.me>
2023-12-23 08:20:27 +0800

    Missing Ziggy

--- resources/js/app.js
+++ resources/js/app.js
@@ -16,7 +16,7 @@ createInertiaApp({
     setup({ el, App, props, plugin }) {
         return createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
+            .use(ZiggyVue, Ziggy)
             .mount(el);
     },
     progress: {
